9ad80da059e4a1cd4947ed3d85f52229692de4e3
224043 <224043@edu.rocfriesepoort.nl>
2023-12-13 15:44:58 +0100

    add os

---
+++ audio.py
@@ -0,0 +1,11 @@
+import os
+import pygame
+
+class Audio:
+
+    def __init__(self) -> None:
+        print ("hi")
+        print (os.getcwd()) 
+
+    def play_sound(self):
+        print ("vf")
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print('hi')
+from audio import Audio
+
+audio = Audio()
+
+audio.play_sound()
