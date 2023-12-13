9cda40394c522755f0b5060d3da948fe743f3ba1
224043 <224043@edu.rocfriesepoort.nl>
2023-12-13 16:07:45 +0100

    added sound

--- audio.py
+++ audio.py
@@ -1,11 +1,21 @@
 import os
 import pygame
 
+
 class Audio:
 
+    oceanSound = "./Audio/ocean sound"
+
     def __init__(self) -> None:
-        print ("hi")
+        pygame.mixer.init(buffer=10000)
+        pygame.mixer.music.set_volume(100)
         print (os.getcwd()) 
 
     def play_sound(self):
-        print ("vf")
+        playing = pygame.mixer.get_busy()
+        if not playing:
+            sound = pygame.mixer.Sound(self.oceanSound)
+            sound.play()
+    
+    def __del__(self):
+        pygame.mixer.quit()
